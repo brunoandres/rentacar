bf8786bbfdbaf60962d7601f3d09a6e24f8cc0ee
Your name here <brunoandres2013@gmail.com>
2019-09-23 16:45:54 -0300

    commit

---
+++ vistas/modulos/check-code.php
@@ -0,0 +1,40 @@
+<section id="portfolio">
+  <div class="container">
+    <div class="center">
+      <h2>Check Code</h2>
+      <p class="lead">Ingrese su cógigo de Reserva para ver el detalle.</p>
+    </div>
+    <div class="row h-100 justify-content-center align-items-center contact-wrap">
+      <div class="col-sm-8 col-sm-offset-2">
+        <div id="sendmessage">Your message has been sent. Thank you!</div>
+        <div id="errormessage"></div>
+        <form method="post" id="formulario">
+          <div class="form-group">
+            <input type="text" name="code" class="form-control" id="code" placeholder="Código de Reserva" data-rule="minlen:4"
+              data-msg="Please enter at least 4 chars" />
+            <div class="validation"></div>
+          </div>
+          <div class="text-center"><button type="submit" id="button" id="button" class="btn btn-success btn-lg">Consultar Código!</button></div>
+        </form>
+        <div id="resultado"></div>
+      </div>
+    </div>
+    <!--/.row-->
+  </div>
+  <!--/.container-->
+</section>
+<!--/#contact-page-->
+<br><br><br><br>
+
+<script>
+  
+ $(document).ready(function(){
+ 	var code = $("#code").val();
+ 	$("button").click(function(){
+ 		$("#resultado").load("load-code-reservation.php",{
+ 			code : code
+ 		})
+ 	});
+ });
+
+</script>
---
+++ vistas/modulos/load-code-reservation.php
@@ -0,0 +1,23 @@
+<?php  
+$enlace = mysqli_connect("localhost", "u756079281_dev", "cavaliere", "u756079281_dev");
+
+$code = $_POST['code'];
+
+$sql = "select * from reservas where codigo = '$code'";
+
+$result = mysqli_query($enlace,$sql);
+
+if (mysqli_num_rows($result) > 0) {
+	while ($row = mysqli_fetch_assoc($result)) {
+		echo "<p>";
+		echo $row['nombre'];
+		echo "<br>";
+		echo $row['fecha_desde'];
+		echo "</p>";
+	}
+}else{
+	echo "NO EXISTE LA RESERVA";
+}
+
+
+?>
