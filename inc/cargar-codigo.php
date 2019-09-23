98f17238e4c47692fcd25bc06c691f7255a51abc
Your name here <brunoandres2013@gmail.com>
2019-09-23 18:09:20 -0300

    commit

--- inc/cargar-codigo.php
+++ inc/cargar-codigo.php
@@ -3,7 +3,7 @@ $enlace = mysqli_connect("localhost", "u756079281_dev", "cavaliere", "u756079281
 
 $code = $_POST['code'];
 
-$sql = "select * from reservas where codigo = '$code'";
+$sql = "select a.*,b.* from reservas a,categorias b where a.codigo = '$code' and a.id_categoria = b.id";
 
 $result = mysqli_query($enlace,$sql);
 
@@ -13,41 +13,27 @@ if (mysqli_num_rows($result) > 0) {
 		echo '<div class="row">
         <div class="col-md-4 order-md-2 mb-4">
           <h4 class="d-flex justify-content-between align-items-center mb-3">
-            <span class="text-muted">Your cart</span>
+            <span class="text-muted">Detalles</span>
             <span class="badge badge-secondary badge-pill">3</span>
           </h4>
           <ul class="list-group mb-3">
             <li class="list-group-item d-flex justify-content-between lh-condensed">
               <div>
-                <h6 class="my-0">Product name</h6>
-                <small class="text-muted">Brief description</small>
+                <h6 class="my-0">Categoria</h6>
+                <small class="text-muted">Categoria seleccionada</small>
               </div>
-              <span class="text-muted">$12</span>
+              <span class="text-muted">"'.$row['categoria'].'"</span>
             </li>
             <li class="list-group-item d-flex justify-content-between lh-condensed">
               <div>
-                <h6 class="my-0">Second product</h6>
-                <small class="text-muted">Brief description</small>
+                <h6 class="my-0">Dias de Alquiler</h6>
+                <small class="text-muted">Cantidad de dias</small>
               </div>
-              <span class="text-muted">$8</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Third item</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between bg-light">
-              <div class="text-success">
-                <h6 class="my-0">Promo code</h6>
-                <small>EXAMPLECODE</small>
-              </div>
-              <span class="text-success">-$5</span>
+              <span class="text-muted">"'.$row['total_dias'].'"</span>
             </li>
             <li class="list-group-item d-flex justify-content-between">
-              <span>Total (USD)</span>
-              <strong>$20</strong>
+              <span>Total Reserva (ARG)</span>
+              <strong>" $ '.$row['tarifa'].'"</strong>
             </li>
           </ul>
         </div>
@@ -72,53 +58,38 @@ if (mysqli_num_rows($result) > 0) {
             </div>
 
             <div class="mb-3">
-              <label for="email">Email <span class="text-muted">Dirección de Email</span></label>
+              <label for="email">Dirección de Email</label>
               <input type="email" class="form-control" id="email" value="'.$row['email'].'" placeholder="you@example.com">
               <div class="invalid-feedback">
                 Please enter a valid email address for shipping updates.
               </div>
             </div>
 
-           
-
             <div class="row">
               <div class="col-md-5 mb-3">
-                <label for="country">Country</label>
-                <select class="custom-select d-block w-100" id="country" required>
-                  <option value="">Choose...</option>
-                  <option>United States</option>
-                </select>
+                <label for="country">Fecha Desde</label>
+                <input type="date" class="form-control" value="'.$row['fecha_desde'].'" readonly>
                 <div class="invalid-feedback">
                   Please select a valid country.
                 </div>
               </div>
               <div class="col-md-4 mb-3">
-                <label for="state">State</label>
-                <select class="custom-select d-block w-100" id="state" required>
-                  <option value="">Choose...</option>
-                  <option>California</option>
-                </select>
+                <label for="date">Fecha Hasta</label>
+                <input type="text" class="form-control" value="'.$row['fecha_hasta'].'" readonly>
                 <div class="invalid-feedback">
                   Please provide a valid state.
                 </div>
               </div>
               <div class="col-md-3 mb-3">
-                <label for="zip">Zip</label>
-                <input type="text" class="form-control" id="zip" placeholder="" required>
+                <label for="zip">Hora Retiro</label>
+                <input type="text" class="form-control" value="'.$row['hora_desde'].'" readonly>
                 <div class="invalid-feedback">
                   Zip code required.
                 </div>
               </div>
             </div>
             <hr class="mb-4">
-            <div class="custom-control custom-checkbox">
-              <input type="checkbox" class="custom-control-input" id="same-address">
-              <label class="custom-control-label" for="same-address">Shipping address is the same as my billing address</label>
-            </div>
-            <div class="custom-control custom-checkbox">
-              <input type="checkbox" class="custom-control-input" id="save-info">
-              <label class="custom-control-label" for="save-info">Save this information for next time</label>
-            </div>
+      
             <hr class="mb-4">
             <hr class="mb-4">
             <button class="btn btn-primary btn-lg btn-block" type="submit">Continuar al Sitio</button>
