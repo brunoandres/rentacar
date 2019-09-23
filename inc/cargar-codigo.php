a408cac458da45861d194b9b3ab918fbb6bba9b9
Your name here <brunoandres2013@gmail.com>
2019-09-23 17:54:06 -0300

    commit

--- inc/cargar-codigo.php
+++ inc/cargar-codigo.php
@@ -9,7 +9,43 @@ $result = mysqli_query($enlace,$sql);
 
 if (mysqli_num_rows($result) > 0) {
 	while ($row = mysqli_fetch_assoc($result)) {
-		echo "<p>";
+
+		echo '<div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
+  <!-- Position it -->
+  <div style="position: absolute; top: 0; right: 0;">
+
+    <!-- Then put toasts within -->
+    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="toast-header">
+        <img src="..." class="rounded mr-2" alt="...">
+        <strong class="mr-auto">Bootstrap</strong>
+        <small class="text-muted">just now</small>
+        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="toast-body">
+        See? Just like this.
+      </div>
+    </div>
+
+    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="toast-header">
+        <img src="..." class="rounded mr-2" alt="...">
+        <strong class="mr-auto">Bootstrap</strong>
+        <small class="text-muted">2 seconds ago</small>
+        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="toast-body">
+        Heads up, toasts will stack automatically
+      </div>
+    </div>
+  </div>
+</div>';
+
+		/*echo "<p>";
 		echo $row['nombre'];
 		echo "<br>";
 		echo $row['fecha_desde'];
@@ -17,12 +53,12 @@ if (mysqli_num_rows($result) > 0) {
 		echo $row['fecha_hasta'];
 		echo "<br>";
 		echo '$ '.$row['tarifa'];
-		echo "</p>";
+		echo "</p>";*/
 	}
 }else{
 	echo "<script>
- 							toastr.error('No existe Reserva con el código ingresado.', 'Error Código', {timeOut: 8000})
- 						</script>";
+				toastr.error('No existe Reserva con el código ingresado.', 'No se encontraron resultados', {timeOut: 8000})
+			</script>";
 }
 
 
--- vistas/modulos/mi-reserva.php
+++ vistas/modulos/mi-reserva.php
@@ -1,17 +1,14 @@
 <section id="portfolio">
   <div class="container">
     <div class="center">
-      <h2>Check Code</h2>
-      <p class="lead">Ingrese su cógigo de Reserva para ver el detalle.</p>
+      <h2>Consultar Mi Reserva</h2>
+      <p class="lead">Ingrese su cógigo de Reserva para ver los detalles.</p>
     </div>
     <div class="row h-100 justify-content-center align-items-center contact-wrap">
       <div class="col-sm-8 col-sm-offset-2">
-        <div id="sendmessage">Your message has been sent. Thank you!</div>
         <div id="errormessage"></div>
           <div class="form-group">
-            <input type="text" name="code" class="form-control" id="code" placeholder="Código de Reserva" data-rule="minlen:4"
-              data-msg="Please enter at least 4 chars" />
-            <div class="validation"></div>
+            <input type="text" name="code" class="form-control" id="code" placeholder="Ingrese el código asigando al confirmar su Reserva" minlength="5" />
           </div>
           <div class="text-center"><button type="submit" name="button" id="button" class="btn btn-success btn-lg">Consultar Código!</button></div>
         <div id="resultado"></div>
@@ -22,4 +19,4 @@
   <!--/.container-->
 </section>
 <!--/#contact-page-->
-<br><br><br><br>
+<br><br><br><br><br><br>
