d59f718c1499481788b8dd55c3cf176d001d55ee
brunoandres <brunoandres2013@gmail.com>
2019-09-25 13:34:17 -0300

    commit

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ require_once "./panel/controladores/controlador.categorias.php";
 require_once "./panel/modelos/modelo.categorias.php";
 require_once "./panel/controladores/controlador.configuraciones.php";
 require_once "./panel/modelos/modelo.configuraciones.php";
-$plantilla = new ControladorPlantilla();
-$plantilla -> ctrPlantilla();
+/*$plantilla = new ControladorPlantilla();
+$plantilla -> ctrPlantilla();*/
 
 
 ?>
